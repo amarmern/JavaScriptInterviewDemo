3e44a68dc1a906e3ffc4de4e181b24d5773559bb
Amrendra Kumar <amarmca51@gmail.com>
2020-08-16 13:43:52 +0530

    Create peristance.js
    adding the persistant interview qustions

---
+++ peristance.js
@@ -0,0 +1,9 @@
+let arr = ["abc", "pqr", "xyz"];
+let arr2 = []
+for(let i =0 ; i < arr.length; i++){
+  console.log(arr[i])
+  let str = arr[i].split('').reverse().join('')
+ arr2.push(str)
+}
+
+console.log(arr2)
