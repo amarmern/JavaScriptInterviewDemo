6b0bccd86016c1021706238d8167381f070e4500
Amrendra Kumar <amarmca51@gmail.com>
2021-07-05 19:08:41 +0530

    Update FindSumElements.js

--- algorithms/math-type-algo/FindSumElements.js
+++ algorithms/math-type-algo/FindSumElements.js
@@ -1,22 +1,21 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arr = [1, 2, 3, 7, 5];
+let sum = 12;
 
-function sumArrElement(arr, n, sum) {
+function sumOfArray(arr, sum) {
   let curr_sum = 0;
-  // Pick a starting point
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i <= arr.length; i++) {
     curr_sum = arr[i];
-    // try all subarrays starting with 'i'
-    for (let j = i + 1; j <= n; j++) {
+    for (let j = i + 1; j <= arr.length; j++) {
       if (curr_sum === sum) {
-        console.log("Sum found between indexes " + i + " and " + (j - 1));
+        console.log(i, j - 1);
         return;
+      } else {
+        curr_sum += arr[j];
       }
-      if (curr_sum > sum || j === n) break;
-      curr_sum = curr_sum + arr[j];
     }
   }
-  console.log("No subarray found");
-  return;
 }
+
+sumOfArray(arr, sum);
 
 console.log(sumArrElement(arr, arr.length, 15));
