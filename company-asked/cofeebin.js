a174215ac42039526945cd581fcc91c0248f94ed
Amrendra Kumar <amarmca51@gmail.com>
2021-07-23 17:20:09 +0530

    Update cofeebin.js

--- company-asked/cofeebin.js
+++ company-asked/cofeebin.js
@@ -25,3 +25,29 @@ function twosum(arr, sum) {
 }
 
 console.log(twosum(arr, sum));
+
+
+
+var twoSum = function(nums, target) {
+    //hash table
+    var hash = {};
+
+    for(let i=0; i<=nums.length; i++){
+      //current number
+        var currentNumber = nums[i];
+        //difference in the target and current number
+        var requiredNumber = target - currentNumber;
+        // find the difference number from hashTable
+        const index2 = hash[requiredNumber];
+
+        // if number found, return index 
+        // it will return undefined if not found
+        if(index2 != undefined) {
+            return [index2, i]
+        } else {
+           // if not number found, we add the number into the hashTable
+            hash[currentNumber] = i;
+
+        }
+    }
+};
