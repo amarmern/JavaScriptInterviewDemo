40c0a7d5d3fc286008daa0cbb25e04dc3b607593
Amrendra Kumar <amarmca51@gmail.com>
2021-07-24 12:27:45 +0530

    Create landt.js

---
+++ company-asked/landt.js
@@ -0,0 +1,40 @@
+
+    console.log(a);
+    let a = 10;
+
+// var a =10;
+// func();
+// function func() {​​​
+// a=20;
+// var a =5;
+// console.log(a);
+// }​​​
+// console.log(a);
+
+//     let arr1 = [1,2,3];
+// const arr2 = arr1;
+// arr1.push(4);
+// console.log(arr1, arr2);
+
+//     if ( {​​​}​​​ ) {​​​
+// console.log('ENTER');
+// }​​​
+// ​console.log(1 + '1',1 - '1',
+// '1' - '1',
+// '1' - 'add',
+// 1 * '1')
+
+//     let person1 = [];
+// let person2 = [];
+// console.log(person1 == person2)
+
+const arr = [1,2,3,4];
+for(var i =0; i < arr.length; i++){
+setTimeout(function(){
+console.log(i, arr[i]);}​​​3000)}​​​
+
+
+//     const arr1 = [1,2,3];
+// const arr2 = arr1;
+// arr1 = [];
+// console.log(arr1, arr2);
