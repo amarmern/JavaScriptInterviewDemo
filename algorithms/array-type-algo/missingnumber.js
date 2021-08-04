2f7377b7a47077c6f33aebd694247a944a037702
Amrendra Kumar <amarmca51@gmail.com>
2021-08-04 22:23:16 +0530

    Update missingnumber.js

--- algorithms/array-type-algo/missingnumber.js
+++ algorithms/array-type-algo/missingnumber.js
@@ -19,15 +19,27 @@ for (var i = 0; i < arrayLength; i++) {
 }
 
 console.log(missing);
+const findMissing = num => {
+  const max = Math.max(...num); // Will find highest number
+  const min = Math.min(...num); // Will find lowest number
+  const missing = []
 
-var numbers = [9, 1, 5, 8, 7, 4, 3, 0, 10, 13, 15, 19, 12, 16, 18]; // Missing  Missing 2,6,11,14,17
-var numbers = numbers.sort((a, b) => a - b);
-var missing = [];
+  for(let i=min; i<= max; i++) {
+    if(!num.includes(i)) { // Checking whether i(current value) present in num(argument)
+      missing.push(i); // Adding numbers which are not in num(argument) array
+    }
+  }
+  return missing;
+}
 
-// Find the missing array items
-// for ( var i = 0; i < numbers.length; i++ ) {
 
-//     if ( (numbers[i+1] - numbers[i]) > 1 ) {
-//         missing.push( numbers[i+1] - numbers[1] );
-//     }
-// }
+var arr = [1,  3, 5, 6,  8];
+console.log(findMissing(arr));
+
+const myFunc = (arr) => {
+  const sum = ((arr.length + 1) * (arr.length + 2)) / 2;
+  const arrSum = (arr) => arr.reduce((a, b) => a + b, 0);
+  return sum - arrSum(arr);
+};
+const myArray = [1, 2, 10, 5, 6, 4, 7, 9, 3, 11];
+console.log(myFunc(myArray));
