a8e32e9a852d6892befba50e0fed5ed2d94fe030
Amrendra Kumar <amarmca51@gmail.com>
2021-08-05 17:02:25 +0530

    Create service-max.js

---
+++ company-asked/service-max.js
@@ -0,0 +1,68 @@
+1. Write the output for the following
+	- "2"+2 
+	- "2"-2 
+	- "2"%2 
+	- "2"*2
+  - "2"**2
+  
+  function add(a){
+  return functon b(){
+  return a + b
+  }
+  
+  let sum  = add(2)(3)
+  
+Implement O Function
+
+var x = O(10);
+console.log(x());  // should print 10
+x(5);
+console.log(x()); // should print 5
+
+function O(num){
+return function(x){
+	return num
+}
+}
+
+
+let arr = [1,2,3,4,5];
+
+arr.map(el=> return ele)
+
+ Aray.prototype.myMap =(arr,callback) =>{
+let newAr = []
+	for(let i =0 ; i < this.length ; i++){
+  	newArr.push(callback(this[i]))
+  }
+}
+
+var newArray =  myMap(arr);
+console.log(newArray);
+
+new Date().time().second();
+
+let Value = setTImeOut(()=>{ Math.random(Math.round(1))},1000)
+console.log(Value)
+
+
+function randomGenerator() {
+var myrandomnumber = Math.random(Math.round(1);
+console.log(myrandomnumber);
+setTImeOut(()=>{ randomGenerator,myrandomnumber*1000}
+}
+
+[1,2,3,4,[5,6,[7,8]]] => [1,2,3,4,5,6,7,8]
+
+let arr1 =[1,2,3,4,[5,6,[7,8]]]
+
+function FlattenArray(arrayObj){
+  for(item of arrayObj){
+  if(Array.isArray(item) == false){
+    result.push(item);
+  }
+  else{
+    FlattenArray(item);
+  }
+  }
+}
