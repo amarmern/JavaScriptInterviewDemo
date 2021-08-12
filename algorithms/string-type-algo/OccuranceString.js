86c59ff2cbbb49a8a4e028632ecee0155cc62841
Amrendra Kumar <amarmca51@gmail.com>
2021-08-12 08:29:43 +0530

    Update OccuranceString.js

--- algorithms/string-type-algo/OccuranceString.js
+++ algorithms/string-type-algo/OccuranceString.js
@@ -1,24 +1,19 @@
 var str = "abcabcac";
-
-//OcuuranceChar(str);
-
-
 function OcuuranceChar(str) {
-    var chars = str.split("");
-    //declare associative key of array
-    var letters_count = {};
-    for (var i = 0; i < chars.length; i++) {
-        if (letters_count[chars[i]] == undefined) {
-            letters_count[chars[i]] = 0;
-        }
-        else {
-            letters_count[chars[i]]++;
-        }
+  var letters_count = {};
+  for (let i = 0; i < str.length; i++) {
+    if (letters_count[str[i]] === undefined) {
+      letters_count[str[i]] = 1;
+    } else {
+      letters_count[str[i]]++;
     }
-    for (var i in letters_count) {
-        console.log(i + ' ' + letters_count[i]);
-    }
+  }
+  for (let i in letters_count) {
+    console.log(i + " " + letters_count[i]);
+  }
 }
+console.log(OcuuranceChar(str));
+
 
 
 
