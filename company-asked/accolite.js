7a7abd46f00582b94476cdb5602e15b6f7034f9a
Amrendra Kumar <amarmca51@gmail.com>
2021-08-13 15:13:05 +0530

    Update accolite.js

--- company-asked/accolite.js
+++ company-asked/accolite.js
@@ -26,3 +26,70 @@ function sum1(a, b) {
 console.log("sumation1", sum1(2)(3));
 
 console.log("sumation2", sum1(2, 3));
+
+var users = [
+  {
+    id: 1,
+    name: "user1"
+  },
+  {
+    id: 2,
+    name: "user2"
+  }
+];
+
+var ages = [
+  {
+    id: 2,
+    age: 25
+  },
+  {
+    id: 1,
+    age: 23
+  }
+];
+
+var companies = [
+  {
+    id: 2,
+    company: "Microsoft"
+  },
+  {
+    id: 1,
+    company: "NASA"
+  }
+];
+// let merged = [];
+
+// for (let i = 0; i < users.length; i++) {
+//   merged.push({
+//     ...users[i],
+//     ...ages[i],
+//     ...companies[i]
+//   });
+// }
+// console.log(merged);
+
+let arr3 = users.map((item, i) =>
+  Object.assign({}, item, ages[i], companies[i])
+);
+
+console.log(arr3);
+//console.log(results);
+
+// expected result=[{
+// "id":1,
+// "name":"user1",
+// "company":"NASA",
+// "age":23
+// },
+// {
+// "id":2,
+// "name":"user2",
+// "company":"Microsoft"
+// "age":25
+// }
+// ]
+
+console.log(Math.random().toString(32));
+
