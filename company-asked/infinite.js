49547d8ebd9b253b69386e4d133b4909e5aa489a
Amrendra Kumar <amarmca51@gmail.com>
2021-08-20 18:47:08 +0530

    Create infinite.js

---
+++ company-asked/infinite.js
@@ -0,0 +1,36 @@
+for (let i = 0; i < 5; i++) {
+  setTimeout(function() { console.log(i); }, i * 1000 );
+  }
+
+  [16:54] Sayyed Chandbasha
+    const obj ={​​​"name":"Amar"}​​​
+​[16:55] Amrendra Kumar (Guest)
+    const obj = bj1
+​[16:56] Sayyed Chandbasha
+    const arr = new Array();
+​[16:57] Amrendra Kumar (Guest)
+    const arr = []
+
+
+    [17:03] Sayyed Chandbasha
+    (function(){​​​
+var a = b = 3;
+}​​​)();
+​[17:05] Sayyed Chandbasha
+    console.llog(0.2+0.3)
+​[17:06] Sayyed Chandbasha
+    arr = [1,2,3,4,5]
+​[17:06] Amrendra Kumar (Guest)
+    aee.splice(2)
+​[17:07] Amrendra Kumar (Guest)
+    arr.splic(2)
+​[17:07] Amrendra Kumar (Guest)
+    arr.splic(1,2)
+​[17:08] Amrendra Kumar (Guest)
+    
+arr.splice(1,2)
+
+​[17:10] Sayyed Chandbasha
+    let person = [{​​​"name": "chand", "age": 29}​​​, {​​​"name": "chand", "age": 29}​​​, {​​​"name": "basha", "age": 29}​​​, {​​​"name": "sayyed", "age": 29}​​​];
+​[17:12] Amrendra Kumar (Guest)
+    if(obj[person] >2) return perosn
