d67f9890c2da6f6a47a9db7968f0ce113320d773
Amrendra Kumar <amarmca51@gmail.com>
2024-02-25 11:35:53 +0530

    Update OccuranceString.js

--- algorithms/string-type-algo/OccuranceString.js
+++ algorithms/string-type-algo/OccuranceString.js
@@ -1,18 +1,18 @@
-var str = "abcabcac";
-function OcuuranceChar(str) {
-  var letters_count = {};
-  for (let i = 0; i < str.length; i++) {
-    if (letters_count[str[i]] === undefined) {
-      letters_count[str[i]] = 1;
-    } else {
-      letters_count[str[i]]++;
+const countRepeatChar = (str) => {
+  let obj = {};
+  if (str) {
+    for (let i = 0; i < str.length; i++) {
+      if (obj[str[i]]) {
+        obj[str[i]] += obj[str[i]];
+      } else {
+        obj[str[i]] = 1;
+      }
     }
+    console.log(obj);
   }
-  for (let i in letters_count) {
-    console.log(i + " " + letters_count[i]);
-  }
-}
-console.log(OcuuranceChar(str));
+};
+countRepeatChar('aabcddeee');
+
 
 
 
