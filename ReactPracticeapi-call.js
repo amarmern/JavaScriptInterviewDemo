a63c2210a47f8b07a45bb8da9c881db11f8ab4b6
Amrendra Kumar <amarmca51@gmail.com>
2024-03-08 08:56:17 +0530

    Create ReactPracticeapi-call.js

---
+++ ReactPracticeapi-call.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://reqres.in/api/users')
+      .then((data) => setUser(data.data.data));
+  }, []);
+
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/)
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       console.log(data);
+  //       setPhotos(data);
+  //     });
+  // }, [])
+  return (
+    <div className="App">
+      {user &&
+        user.map((p) => (
+          <li key={p.id}>
+            {' '}
+            <div className="card">
+              <img src={p.avatar} alt="Avatar" />
+              <div className="container">
+                <b>
+                  <label>{p.first_name}</label>
+                  <label>{p.last_name}</label>
+                </b>
+              </div>
+              {p.em}
+            </div>
+          </li>
+        ))}
+    </div>
+  );
+}
+
+export default App;
