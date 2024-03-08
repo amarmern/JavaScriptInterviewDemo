8cca9a7f51ae7829e9df5c2f3dd1300d1df11352
Amrendra Kumar <amarmca51@gmail.com>
2024-03-08 09:21:01 +0530

    Update altimerik.js

--- company-asked/altimerik.js
+++ company-asked/altimerik.js
@@ -46,19 +46,18 @@ console.log(totalScore)
 //function pull all 0s left and 1s right
 
 let arr = [0, 1, 0, 1, 1, 1];
-function segrigate01(arr) {
-  let left = 0,
-    right = arr.length - 1;
 
+function segeration01(arr) {
+  let left = 0;
+  let right = arr.length - 1;
   while (left < right) {
-    /* Increment left index while we see 0 at left */
-    while (arr[left] === 0 && left < right) left++;
+    while (arr[left] === 0) {
+      left++;
+    }
+    while (arr[right] === 1) {
+      right--;
+    }
 
-    /* Decrement right index while we see 1 at right */
-    while (arr[right] === 1 && left < right) right--;
-
-    /* If left is smaller than right then there is a 1 at left
-      and a 0 at right. Exchange arr[left] and arr[right]*/
     if (left < right) {
       arr[left] = 0;
       arr[right] = 1;
@@ -66,9 +65,11 @@ function segrigate01(arr) {
       right--;
     }
   }
+  return arr;
 }
 
-console.log(segrigate01(arr));
+console.log(segeration01(arr));
+
 
 
 
